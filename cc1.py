d74f97c6cc4b1c9de8c5856c6f5c59f271f06aee
Natalie Wheeler <120809649+natalieinTX@users.noreply.github.com>
2024-12-27 15:49:27 -0600

    Add files via upload

---
+++ cc1.py
@@ -0,0 +1,19 @@
+#Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a)) 
+
+b = 7.5
+print(type(b))
+
+c = "Howdy, There!"
+print(type(c))
+
+d = a + b
+print(d)
+
+
+
+
